7be7f1304f2f8dff6cd2734c089745eba1e38892
Roland Metivier <metivier.roland@chlorophyt.us>
2021-03-29 09:54:21 -0500

    add drafts for a texturecell not a shader

---
+++ src/main/scala/broccoli/TextureCell.scala
@@ -0,0 +1,85 @@
+// See README.md for license details.
+
+package broccoli
+
+import chisel3._
+import chisel3.util._
+
+class TextureCell extends Module {
+  val io = IO(new Bundle {
+    val aresetn = Input(Bool())
+    val data = Input(UInt(8.W))
+    val enable = Input(Bool())
+    val strobe = Input(Bool())
+    
+    val hBlank = Input(Bool())
+    val vBlank = Input(Bool())
+    val currentX = Input(UInt(12.W))
+    val currentY = Input(UInt(12.W))
+
+    val textureCoordsA = Input(UInt(16.W))
+    val textureCoordsB = Input(UInt(16.W))
+    val textureCoordsC = Input(UInt(16.W))
+    val textureCoordsD = Input(UInt(16.W))
+
+    val address = Input(UInt(8.W))
+    val write = Input(Bool())
+    val ready = Output(Bool())
+  })
+
+  withReset(~io.aresetn) {
+    val state = Reg(UInt(10.W))
+    val programCounter = Reg(UInt(16.W))
+    val write = Reg(Bool())
+
+    // Captured on state[0]
+    val textureMatrix = Reg(VecInit.tabulate(4)(UInt(16.W)))
+    val textureCoordX = Reg(UInt(12.W))
+    val textureCoordY = Reg(UInt(12.W))
+    val textureMemory = SyncReadMem(256, UInt(8.W)) // 16*16 texture memory
+    // ========================================================================
+    //  STATE MACHINE
+    // ========================================================================
+    when(~io.aresetn) {
+      state := 0.U(10.W)
+    }.elsewhen(io.strobe & ~state.orR()) {
+      state := 1.U(10.W)
+    }.elsewhen(io.enable) {
+      state := state << 1.U(10.W)
+    }
+    // ========================================================================
+    //  Affine Transformation
+    // ========================================================================
+    // using matrix-vector multiply:
+    // | a b | * | x | --> | x'|
+    // | c d | * | y | --> | y'|
+    //
+    // ax * by = x'
+    // cx * dy = y'
+    // // TODO: Signed or unsigned handler?
+    // === Hold Matrix Element A ===
+    when(~io.aresetn) {
+      textureMatrix(0) := "h0080".U(16.W)
+    }.elsewhen(io.enable & io.write & io.strobe & state(0))
+      textureMatrix(0) := io.textureCoordsA
+    }
+    // === Hold Matrix Element B ===
+    when(~io.aresetn) {
+      textureMatrix(1) := "h0000".U(16.W)
+    }.elsewhen(io.enable & io.write & io.strobe & state(0))
+      textureMatrix(1) := io.textureCoordsB
+    }
+    // === Hold Matrix Element C ===
+    when(~io.aresetn) {
+      textureMatrix(2) := "h0000".U(16.W)
+    }.elsewhen(io.enable & io.write & io.strobe & state(0))
+      textureMatrix(2) := io.textureCoordsC
+    }
+    // === Hold Matrix Element D ===
+    when(~io.aresetn) {
+      textureMatrix(3) := "h0080".U(16.W)
+    }.elsewhen(io.enable & io.write & io.strobe & state(0))
+      textureMatrix(3) := io.textureCoordsD
+    }
+  }
+}
