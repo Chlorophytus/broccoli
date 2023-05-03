9131ea6256d88fc4a0444120b81aded69670c5c7
Roland Metivier <metivier.roland@chlorophyt.us>
2023-05-02 21:33:39 -0500

    LLVM circt ought to work now

--- src/main/scala/broccoli/Verilog.scala
+++ src/main/scala/broccoli/Verilog.scala
@@ -1,13 +1,15 @@
 package broccoli
 
-import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
+import chisel3.stage.ChiselGeneratorAnnotation
+import circt.stage.ChiselStage
+
 import com.typesafe.scalalogging._
 
 object Verilog extends App with LazyLogging {
   logger.info("Generating Verilog")
 
-  (new circt.stage.ChiselStage).execute(
-    Array("-X", "verilog"),
+  (new ChiselStage).execute(
+    Array("--target", "verilog"),
     Seq(ChiselGeneratorAnnotation(() => new Broccoli))
   )
 }
